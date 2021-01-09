ab10b4a0867ec090b9f092417ba4c8b0af2201e1
vichunani <viru@list.ru>
2021-01-09 23:11:52 +0600

    2commit

---
+++ controllers/AppController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+    public function beforeAction($action){
+
+        $this->view->title = \Yii::$app->name;
+
+        return parent::beforeAction($action);
+    }
+}
