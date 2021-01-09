d432f807fdafcc1cf46804f1c7e053f7fabad43b
vichunani <viru@list.ru>
2021-01-09 12:36:33 +0600

    first commit2

--- config/web.php
+++ config/web.php
@@ -43,14 +43,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+      
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => false,
             'rules' => [
             ],
         ],
-        */
+     
     ],
     'params' => $params,
 ];
