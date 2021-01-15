9a83183a69381bdc8015abad7102e90093f164f8
vichunani <viru@list.ru>
2021-01-15 10:09:29 +0600

    4commit

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace app\controllers;
 
+use app\models\Product;
 use app\controllers\AppController;
 
 class HomeController extends AppController
 {
     public function actionIndex(){
+        
+        $offers = Product::find()->where(['is_offer' => 1])->limit(4)->all();
 
-        return $this->render('index');
+
+        return $this->render('index', compact('offers'));
     }
 }
--- views/site/error.php
+++ views/site/error.php
@@ -9,19 +9,20 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
+<div class="banner">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <?= $this->render('//layouts/inc/sidebar') ?>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+    <div class="w31_banner_nav_right">
+        <div style="padding: 6em 5em;">
+            <h2><?= Html::encode($this->title)?></h2>
+            <div>
+                <?= nl2br(Html:: encode($message))?>
+            </div>
+        </div>
     </div>
+    <div class="clearfix"></div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+  
 
 </div>
